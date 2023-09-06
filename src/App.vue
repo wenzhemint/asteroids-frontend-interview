0dcf9a335b3bb79e8aced87361ce6502e49a61b8
wenzhemint <wenzhemint@outlook.com>
2023-09-07 00:51:19 +0800

    Connect Socket server.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <ConnectionState />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ConnectionState from './components/ConnectionState.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    ConnectionState
   }
 }
 </script>
