bb566d8decf1affc1f653f0d6d58a2ed77f007a0
wenzhemint <wenzhemint@outlook.com>
2023-09-07 01:17:32 +0800

    Create Router.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <ConnectionState />
+  <div>
+    <router-view />
+  </div>
 
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import ConnectionState from './components/ConnectionState.vue'
+// import ConnectionState from './components/ConnectionState.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    ConnectionState
+    // ConnectionState
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ src/router.js
@@ -0,0 +1,17 @@
+import { createWebHistory, createRouter } from "vue-router";
+import HomePage from "./components/HomePage/HomePage.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: HomePage
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
